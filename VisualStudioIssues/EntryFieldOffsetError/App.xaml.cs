2a83dcdf8f3a3de5ac92dda06054ae65f7c70fa2
Martin Stromberg <m.stromberg@live.de>
2022-09-18 19:04:21 +0200

    EntryFieldOffsetError

---
+++ VisualStudioIssues/EntryFieldOffsetError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EntryFieldOffsetError
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
